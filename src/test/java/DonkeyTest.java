0f5aa8caee06546884f7eddd3b31f097d1a7ddba
Pete J <petesaiacct@gmail.com>
2021-09-27 16:21:30 -0400

    test

--- src/test/java/DonkeyTest.java
+++ src/test/java/DonkeyTest.java
@@ -31,16 +31,13 @@ public class DonkeyTest {
         h.upgradeSpeed();
         assertEquals(2, h.getMaxSpeed());
     }
-
     @Test(timeout = 50)
     public void TestDowngradeSpeed() {
         h.downgradeSpeed();
         assertEquals(0, h.getMaxSpeed());
     }
-
     @Test(timeout = 50)
     public void TestGetPrice() {
         assertEquals(10, h.getPrice());
     }
-
 }
